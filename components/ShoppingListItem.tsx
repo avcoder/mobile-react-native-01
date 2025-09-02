6ce7857fadb395802f525b563b62d524fefec179
avcoder <avcoder@users.noreply.github.com>
2025-09-02 18:25:19 -0400

    edit

--- components/ShoppingListItem.tsx
+++ components/ShoppingListItem.tsx
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
     color: theme.colorGrey,
   },
   completedButton: {
-    backgroundColor: theme.colorGrey,
+    backgroundColor: theme.colorLightGrey,
   },
   button: {
     backgroundColor: theme.colorWhite,
