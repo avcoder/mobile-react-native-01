583c82fe1be9dbd0b34b1a40bbe4b8ef4b85791b
avcoder <avcoder@users.noreply.github.com>
2025-09-04 10:42:36 -0400

    edit

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,55 @@
 import { StyleSheet, TextInput, View } from "react-native";
 import ShoppingListItem from "../components/ShoppingListItem";
 import { theme } from "./theme";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted?: boolean;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "apples" },
+  { id: "2", name: "bananas" },
+  { id: "3", name: "carrots" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toISOString(), name: value },
+        ...shoppingList,
+      ];
+
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TextInput placeholder="Enter item" style={styles.textInput} />
-      <ShoppingListItem name="Apples" />
-      <ShoppingListItem name="Bananas" isCompleted={true} />
-      <ShoppingListItem name="Carrots" isCompleted />
+      <TextInput
+        placeholder="Enter item"
+        style={styles.textInput}
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+
+      {shoppingList.map((item) => (
+        <ShoppingListItem
+          name={item.name}
+          isCompleted={item.isCompleted}
+          key={item.id}
+        />
+      ))}
     </View>
   );
 }
@@ -17,6 +58,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingTop: 12,
   },
   textInput: {
     borderColor: theme.colorLightGrey,
