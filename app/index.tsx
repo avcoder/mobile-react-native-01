4d2249966f28e33980fe3b570d32870bbef6f4e8
avcoder <avcoder@users.noreply.github.com>
2025-09-04 10:27:56 -0400

    edit

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import ShoppingListItem from "../components/ShoppingListItem";
+import { theme } from "./theme";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput placeholder="Enter item" style={styles.textInput} />
       <ShoppingListItem name="Apples" />
       <ShoppingListItem name="Bananas" isCompleted={true} />
       <ShoppingListItem name="Carrots" isCompleted />
@@ -15,6 +17,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
